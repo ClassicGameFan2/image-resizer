d8b037bf415dbe461f65bee21810795ef2fb5e41
ClassicGameFan2 <johnleroy55@tutamail.com>
2026-03-25 04:23:12 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Image Resizer is running!" << std::endl;
+    std::cout << "Cloud compiler setup was successful." << std::endl;
+    return 0;
+}
